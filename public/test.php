c5ee1caeb74cb002a805a96c8ddb9ab9ed917dab
Jakub Igla <jakub.igla@valtech.co.uk>
2015-09-06 20:26:54 +0200

    php fpm

--- public/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$rec = ['MOT2-01-20150904-112357-1A19E151', null, 'MOT2-01-20150904-113344-473EB584', 'MOT2-01-20150904-113446-6605C6D3'];
-
-$arr = ['data' => array_filter($rec)];
-
-var_dump(count($rec));
-var_dump(json_encode($arr)) . PHP_EOL;
